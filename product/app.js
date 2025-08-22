6c20be27e75262a5a860606336b2a32cd529c65d
mfyd-12 <fsdvshnsv657@gmail.com>
2025-08-22 16:48:02 +0300

    update

--- product/app.js
+++ product/app.js
@@ -1,15 +1,15 @@
 // app.js
 document.addEventListener("DOMContentLoaded", () => {
-  const productsList = document.querySelector(".products-list");
-
-  // Using a free dummy API for fake products
+const productsList = document.querySelector(".products");
+  
+// Using a free dummy API for fake products
   fetch("http://localhost:3000/products")
     .then(response => response.json())
     .then(products => {
       productsList.innerHTML = products.map(prod => `
-        <div class='product'>
+        <div class='qaqa'>
           <img src="${prod.image}" />
-            <h2>${prod.title}</h2>
+            <h4>${prod.title}</h4>
             <p>${prod.description}</p>
             <p>Price: $${prod.price}</p>
             <button class="add-to-cart">Add to Cart</button>
