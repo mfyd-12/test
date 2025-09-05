f38e633760d96ffc46fa62ee165d906515d365d0
mfyd-12 <fsdvshnsv657@gmail.com>
2025-09-05 15:30:23 +0300

    update

--- xo/xo.js
+++ xo/xo.js
@@ -1,12 +1,20 @@
     let currentPlayer = "❌"; 
-
+    let xs=[]
+    let os=[]
     const buttons = document.querySelectorAll(".call");
 
     buttons.forEach(button => {
       button.addEventListener("click", () => {
         if (button.textContent === "") { 
           button.textContent = currentPlayer; 
-          currentPlayer = currentPlayer === "❌" ? "⭕" : "❌";
+             if(currentPlayer==="❌"){
+              xs.push(currentPlayer)
+            currentPlayer="O"
+          }else{
+            os.push(currentPlayer)
+            currentPlayer="❌"
+          }        
         }
-      });
+
+if(button.textContent==="!")      });
     });
